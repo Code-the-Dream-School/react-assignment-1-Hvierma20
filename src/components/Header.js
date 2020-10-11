377dff6bf88702ef23069a77d5e130153e48b8e0
Hermes Vierma <Herminho@Hermess-MacBook-Air.local>
2020-10-11 12:15:40 -0500

    React first assignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import logo from '../images/logo.png';
 
 
 const Header = (props) => {
-  return();
+
+  let button;
+  function handleStart(){
+    props.changeStart();
+  }
+  if(!props.start)
+  button = <button className = "btn btn-dark niceFont" onClick={handleStart}> Start </button>
+
+  return(
+    <Fragment>
+     <div class = "center">
+          <h1 class = "niceFont" >Tic Tac Toe</h1>
+          <img src={logo} alt="Tic Tac Toe" style= {{ width: '120px' }} />
+          <button className="btn-primary btn-dark niceFont" type="button">{button}</button>
+      </div>
+     
+    </Fragment>
+  );
 }
 
 export default Header;
